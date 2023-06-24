8f9747985c844d45ef27ef86c08df2c6eea348dc
Valentina Bowman <yogurlval@gmail.com>
2023-06-24 17:26:32 -0600

    up to part 4 completed

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -11,25 +11,27 @@ afterEach(async () => {
 });
 
 describe("Duel Duo tests", () => {
-  // test("page loads with title", async () => {
-  //   await driver.get("http://localhost:8000");
-  //   await driver.wait(until.titleIs("Duel Duo"), 1000);
-  // });
+  test("page loads with title", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.wait(until.titleIs("Duel Duo"), 1000);
+  });
 
-  // test("clicking Draw button displays div with id='choices'", async ()=>{
-  //   await driver.get("http://localhost:8000");
-  //   const drawButton = await driver.findElement(By.id("draw"));
-  //   await drawButton.click();
-  //   const divChoices = await driver.findElement(By.id("choices"));
-  //   expect(await divChoices.isDisplayed()).toBe(true);
-  //   await driver.sleep(3000);
-  // });
+  test("clicking Draw button displays div with id='choices'", async ()=>{
+    await driver.get("http://localhost:8000");
+    const drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    const divChoices = await driver.findElement(By.id("choices"));
+    expect(await divChoices.isDisplayed()).toBe(true);
+    await driver.sleep(3000);
+  });
 
   test("when bot is 'Removed from Duo' it goes back to 'choices'", async ()=>{
     await driver.get("http://localhost:8000");
-    const addBot = await driver.findElement(By.className("bot-btn"));
+    const drawBots = await driver.findElement(By.id("draw"));
+    await drawBots.click();
+    const addBot = await driver.findElement(By.xpath("//button[text()='Add to Duo']"))
     await addBot.click();
-    const removeBot = await driver.findElement(By.className("bot-btn"));
+    const removeBot = await driver.findElement(By.xpath("//button[text()='Remove from Duo']"))
     const duoContainer = await driver.findElement(By.id("player-duo"));
     await driver.sleep(3000)
     await removeBot.click();
